b87016d6115a6d259eaa089d7def31e199e96af8
maja <maja07300071@gmail.com>
2026-05-26 16:30:31 +0800

    1

---
+++ homework_5/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace homework_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 23);
+            label1.TabIndex = 0;
+            label1.Text = "請選擇生日";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(40, 111);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(300, 30);
+            dateTimePicker1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private DateTimePicker dateTimePicker1;
+    }
+}
---
+++ homework_6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Dynamic;
+
+namespace homework_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal taxRate = 0;
+            decimal progressiveDifference = 0;
+            decimal totalTax = 0;
+
+            Console.Write("請輸入年收入：");
+            decimal money = decimal.Parse(Console.ReadLine());
+
+            if (money<0)
+            {
+                Console.WriteLine("請勿輸入0以下的數字");
+            }
+
+            if (money <= 540000)
+            {
+                taxRate = 0.05m;
+                progressiveDifference = 0;
+            }
+            else if (money <= 1210000)
+            {
+                taxRate = 0.12m;
+                progressiveDifference = 37800;
+            }
+            else if (money <= 2420000)
+            {
+                taxRate = 0.20m;
+                progressiveDifference = 134600;
+            }
+            else if (money <= 4530000)
+            {
+                taxRate = 0.30m;
+                progressiveDifference = 376600;
+            }
+            else if (money <= 10310000)
+            {
+                taxRate = 0.40m;
+                progressiveDifference = 829600;
+            }
+            else
+            {
+                taxRate = 0.50m;
+                progressiveDifference = 1860600;
+            }
+
+            totalTax = (money * taxRate) - progressiveDifference;
+
+            Console.WriteLine(totalTax);
+        }
+    }
+}
