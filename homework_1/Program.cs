f40a80a693d3b1ed03c245b4270dd659778853e0
maja <maja07300071@gmail.com>
2026-05-26 12:19:36 +0800

    加入專案檔案。

---
+++ homework_1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入一個大於90的數字：");
+            int max = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= max; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("Dann");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("School");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Build");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
