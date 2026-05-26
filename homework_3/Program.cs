89bb8e333395d3d4c1d99efb259c46d16c7903e6
maja <maja07300071@gmail.com>
2026-05-26 14:33:29 +0800

    1

---
+++ homework_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace homework_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入有意義的英文敘述字串");
+            string words = Console.ReadLine();
+            var result = words.ToLower().Split(' ').GroupBy(w => w);
+
+            Console.WriteLine(string.Join("\n",result.Select(x => $"{x.Key} : {x.Count()}")));
+        }
+    }
+}
